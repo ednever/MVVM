8cd74f3b2222c4d0f821df84ce0b520c18d10a60
opilane <opilane@HP-CZC2349HT0>
2023-04-20 09:54:43 +0300

    +++

--- MVVM/Views/FriendPage.xaml.cs
+++ MVVM/Views/FriendPage.xaml.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MVVM.ViewModels;
 
 namespace MVVM.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FriendPage : ContentPage
     {
-        public FriendPage()
+        public FriendPage(FriendViewModel vm)
         {
             InitializeComponent();
+            //ViewModel = vm;
+            //this.BindingContext = ViewModel;
         }
     }
 }
